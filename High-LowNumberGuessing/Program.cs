e8c4100dde99dd1412e17df168edba713f6cfc0d
Raymond Hart <z3nithvip3r@gmail.com>
2023-10-07 06:58:08 -0500

    Output if guess too high/low

--- High-LowNumberGuessing/Program.cs
+++ High-LowNumberGuessing/Program.cs
@@ -14,22 +14,36 @@ namespace ConsoleApp
             while (guesses > 0)
             {
                 
-                Console.WriteLine("Guess a number from 0-100. You have " + guesses + " guesses left. Good luck");
+                Console.WriteLine($"Guess a number from 0-100. You have {guesses} guesses left. Good luck!");
                 userInput = Int32.Parse(Console.ReadLine());
 
                 if (userInput == randomNumber)
                 {
-                    Console.WriteLine("You guessed correctly");
+                    Console.WriteLine("You guessed correctly.");
                     break;
                 }
                 else
                 {
-                    if (guesses > 1)
-                    Console.WriteLine("You guessed incorrectly, try again.");
-                    guesses--;
+                    if (guesses > 0) 
+                    { 
+                    if (userInput > randomNumber)
+                        {
+                            Console.WriteLine("You guessed incorrectly. The number you guessed was too high.");
+                            guesses--;
+                        }
+                    if (userInput < randomNumber)
+                        {
+                            Console.WriteLine("You guessed incorrectly. The number you guessed was too low.");
+                            guesses--;
+                        }
+                    }
                 }
-            }
 
+            }
+            if (guesses == 0)
+            {
+                Console.WriteLine($"Sorry, you've run out of guesses. The correct number was {randomNumber}.");
+            }
 
         }
     }
